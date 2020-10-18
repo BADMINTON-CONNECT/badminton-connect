ca3d07d1ab41bdcffedffda9f38c7043f4f682cc
Neo Tsai <neotsai@alumni.ubc.ca>
2020-10-18 00:08:52 -0700

    get requestes works but delete doesn't. POST still needs work

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,13 @@ const db = mysql.createConnection({
 
 db.connect();
 
+// get time of server, part of M5
+app.get('/time', (req, res) => {
+  var date = new Date();
+  res.send(date.toString())
+});
+
+// get all users
 app.get('/users', (req, res) => {
   const sql = 'SELECT * FROM users';
 
@@ -24,10 +31,34 @@ app.get('/users', (req, res) => {
   });
 });
 
-app.get('/time', (req, res) => {
-  var date = new Date();
-  res.send(date.toString())
-})
+// get specific user
+app.get('/users/:id', (req, res) => {
+  const sql = 'SELECT * FROM users WHERE user_id = ?';
+
+  db.query(sql, [req.params.id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// delete an user: needs to use POSTMAN 
+app.delete('/users/:id', (req, res) => {
+  const sql = 'DELETE FROM users WHERE user_id = ?';
+
+  db.query(sql, [req.params.id], (err, result) => {
+    if (err) throw err;
+    res.send("Deleted succesfully");
+  });
+});
+
+app.post('/users/add', (req, res) => {
+  let data = [req.body.name, req.body.username, req.body.email];
+  const sql = 'INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)';
+  db.query(sql, data, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
