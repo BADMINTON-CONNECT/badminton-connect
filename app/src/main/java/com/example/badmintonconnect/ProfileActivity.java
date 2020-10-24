3e5b847cc873c43e70e14e87235c651ab519b6c4
Neo Tsai <neotsai@alumni.ubc.ca>
2020-10-23 21:35:55 -0700

    working skeleton code to send a post request to backend

--- app/src/main/java/com/example/badmintonconnect/ProfileActivity.java
+++ app/src/main/java/com/example/badmintonconnect/ProfileActivity.java
@@ -2,11 +2,87 @@ package com.example.badmintonconnect;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class ProfileActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
+
+
+        /*
+        How I'd make the request to the server
+        This is a post request to add a user to the users database
+        -> do this whenever you first sign in i guess
+         */
+        final TextView textView = (TextView) findViewById(R.id.textView2);
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://40.88.38.140:8080/users";
+            JSONObject jsonBody = new JSONObject();
+            // this is the json body that backend would use to get information
+            jsonBody.put("first_name", "Naomi");
+            jsonBody.put("last_name", "Chen");
+            jsonBody.put("email", "naomi@gmail.com");
+            final String mRequestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    textView.setText(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText(error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        //responseString = String.valueOf(response.statusCode);
+                        return super.parseNetworkResponse(response);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
