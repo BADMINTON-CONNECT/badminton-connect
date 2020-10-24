290569c48541a66fd96f6ffe8afb5af2bea38e33
Neo Tsai <neotsai@alumni.ubc.ca>
2020-10-23 21:37:27 -0700

    dealing with 2 databases now. and code example for dealing with FCM

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+//var admin = require('firebase-admin');
 const bodyparser = require('body-parser');
 const port = 8080;
 
 app.use(bodyparser.json());
+
+/*
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
+*/
 
 const db = mysql.createConnection({
 	host: 'localhost',
@@ -23,7 +31,9 @@ app.get('/time', (req, res) => {
 	res.send(date.toString())
 })
 
-
+/*
+User tables requests
+*/
 
 // get all users
 app.get('/users', (req, res) => {
@@ -56,7 +66,7 @@ app.delete('/users/:id', (req, res) => {
 	});
 });
 
-//Insert an employees
+//Insert an user
 app.post('/users', (req, res) => {
     var body = req.body;
     const sql = 'INSERT INTO users (first_name, last_name, email) values (?, ?, ?)';
@@ -72,7 +82,7 @@ app.put('/users/:id', (req, res) => {
 	const sql = 'UPDATE users SET email = ? WHERE user_id = ?';
 	//UPDATE `m5trial`.`users` SET `first_name` = 'David', `last_name` = 'Kang' WHERE (`id` = '5'); -> this updates multiple thigns at once
 	/*
-	This calls a function to do data parse basically -> need to create in the database
+	This calls a "procedure" to do data parse basically -> need to create procedure in the database
 	var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
 	*/
@@ -82,7 +92,79 @@ app.put('/users/:id', (req, res) => {
     })
 });
 
+/*
+Court availability table 
+*/
+
+// get all courts availability for all dates
+app.get('/courts', (req, res) => {
+	const sql = 'SELECT * FROM courts';
+  
+	db.query(sql, (err, result) => {
+	  if (err) throw err;
+	  res.send(result);
+	});
+});
+
+// get specific date
+app.get('/courts/:year/:month/:date', (req, res) => {
+	var sql = 'SELECT * FROM courts WHERE year = ? AND month = ? AND date = ?';
+	db.query(sql, [req.params.year, req.params.month, req.params.date], (err, result) => {
+	  if (err) throw err;
+	  res.send(result);
+	});
+});
+
+//Update an court availability 
+app.put('/courts', (req, res) => {
+    var body = req.body;
+	const sql = 'UPDATE courts SET time_slot1 = ?, time_slot2 = ?, time_slot3 = ?, time_slot4 = ? WHERE court_id = ?';
+    db.query(sql, [body.time_slot1, body.time_slot2, body.time_slot3, body.time_slot4, body.court_id], (err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+/* example
+// getting data from client 
+app.post('/client', (req, res) => {
+    var body = req.body;
+	
+	console.log("Got this from the client: " + body.first_name)
+	console.log("Got this from the client: " + body.last_name)
+	console.log("Got this from the client: " + body.email)
+
+	res.send("got it!!");
+});
+*/
+
+/*
+// This registration token comes from the client FCM SDKs.
+var registrationToken = 'YOUR_REGISTRATION_TOKEN';
+
+var message = {
+  data: {
+    score: '850',
+    time: '2:45'
+  },
+  token: registrationToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+  */
