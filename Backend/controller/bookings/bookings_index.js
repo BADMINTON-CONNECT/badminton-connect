f24713b58bd731f5b6f16fe0ed51b316448bd3c9
Neo Tsai <neotsai@alumni.ubc.ca>
2020-10-27 22:17:58 -0700

    Added date check for push notification

--- Backend/controller/bookings/bookings_index.js
+++ Backend/controller/bookings/bookings_index.js
@@ -1,3 +1,9 @@
+/*
+
+THIS IS BOOKINGS INDEX
+
+*/
+
 const express = require('express')
 const router = express.Router()
 const db = require('../../database/mysql')
@@ -33,5 +39,33 @@ router.post('/', (req, res) => {
         res.send(result)
     })
 });
+
+
+
+function check_bookings_near() {
+	date = new Date();
+	//console.log(date.toString());
+	const current_year = date.getFullYear();
+	const current_month = date.getMonth(); // month is 0 indexed
+	const current_date = date.getDate();
+
+	const sql = 'SELECT DISTINCT users.Registration_Token FROM users INNER JOIN bookings ON users.user_id = bookings.user_id WHERE bookings.Year = ? and bookings.Month = ? and bookings.Date = ?';
+
+	db.query(sql, [current_year, current_month+1, current_date], (err, row) => {
+		if (err) throw err;
+		for(var r in row) {
+			//console.log(row[r].Registration_Token);
+			if (row[r].Registration_Token != null) {
+				admin.sendPushNotification(row[r].Registration_Token, 'Booking Notification', 'Your booking is coming up today!');
+			}
+		}
+	})
+	
+}
+
+
+
+//setInterval(check_bookings_near, 1500); // this is in ms
+check_bookings_near();
 
 module.exports = router
