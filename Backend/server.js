23c02b76bfa5fa4d58e53989bdf0e660bc4c9074
Neo Tsai <neotsai@alumni.ubc.ca>
2020-11-03 14:59:28 -0800

    Fixed some more

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,16 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 const port = 8080;
 
-const db = require('./database/mysql')
+const db = require("./database/mysql")
 const admin = require("./firebase/notification");
-const timeController = require('./controller/time/time_index');
-const usersController = require('./controller/users/users_index');
-const courtsController = require('./controller/courts/courts_index');
-const bookingsController = require('./controller/bookings/bookings_index');
-const availabilityController = require('./controller/availability/availability_index');
+const timeController = require("./controller/time/time_index");
+const usersController = require("./controller/users/users_index");
+const courtsController = require("./controller/courts/courts_index");
+const bookingsController = require("./controller/bookings/bookings_index");
+const availabilityController = require("./controller/availability/availability_index");
 
 
 app.use(bodyparser.json());
@@ -21,19 +21,19 @@ Endpoints
 */
 
 // getting the server time, part of M5
-app.use('/time', timeController);
+app.use("/time", timeController);
 
 // dealing with the useres table from database 
-app.use('/users', usersController);
+app.use("/users", usersController);
 
 // dealing with the courts table from database 
-app.use('/courts', courtsController);
+app.use("/courts", courtsController);
 
 // dealing with the bookings table from database 
-app.use('/bookings', bookingsController);
+app.use("/bookings", bookingsController);
 
 // dealing with the availability table from database 
-app.use('/availability', availabilityController);
+app.use("/availability", availabilityController);
 
 
 app.listen(port, () => {
