87864debf890fa8c7f9f5b8dcb0fc87e5f72ed9f
nchen99 <nathanielle.chen@gmail.com>
2020-11-07 18:24:07 -0800

    deleting old files

--- app/src/main/java/com/example/badmintonconnect/UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.badmintonconnect;
-
-public class UserInfo {
-    private static String userId;
-    public static String getUserId() {return userId;}
-    public static void setUserId(String userId) {
-        UserInfo.userId = userId;
-    }
-}
