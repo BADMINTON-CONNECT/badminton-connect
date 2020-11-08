25cd699fc904d8503fe215840037f16cd5ad65cd
nchen99 <nathanielle.chen@gmail.com>
2020-11-07 18:56:54 -0800

    fixing codacy issues

--- app/src/androidTest/java/com/example/badmintonconnect/BookingActivityTest.java
+++ app/src/androidTest/java/com/example/badmintonconnect/BookingActivityTest.java
@@ -18,83 +18,96 @@ import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Assert;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class ProfileActivityTest {
+public class BookingActivityTest {
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Test
-    public void profileActivityTest() {
+    public void bookingActivityTest() {
         ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.imageButtonProfile), withContentDescription("TODO"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
+                allOf(withId(R.id.imageButtonBooking),
                         isDisplayed()));
         appCompatImageButton.perform(click());
 
-        ViewInteraction appCompatImageButton2 = onView(
-                allOf(withId(R.id.imageButtonEdit), withContentDescription("TODO"),
+        ViewInteraction button = onView(
+                allOf(withId(R.id.checkDateButton), withText("Check Availability of Date!"),
+                        childAtPosition(
+                                allOf(withId(R.id.scrollView2),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction spinner = onView(
+                allOf(withId(R.id.time_slot1),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                8),
+                                7),
                         isDisplayed()));
-        appCompatImageButton2.perform(click());
+        spinner.perform(click());
 
-        ViewInteraction appCompatSpinner = onView(
-                allOf(withId(R.id.spinnerUserSkillLevel),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatSpinner.perform(click());
-
-        DataInteraction appCompatCheckedTextView = onData(anything())
+        DataInteraction checkedTextView = onData(anything())
                 .inAdapterView(childAtPosition(
                         withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
                         0))
                 .atPosition(1);
-        appCompatCheckedTextView.perform(click());
+        checkedTextView.perform(click());
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.buttonSave), withText("Save"),
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.bookingButton3), withText("Book Now"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                12),
+                                14),
                         isDisplayed()));
-        appCompatButton.perform(click());
+        button2.perform(click());
 
         ViewInteraction textView = onView(
-                allOf(withId(android.R.id.text1), withText("2"),
-                        withParent(allOf(withId(R.id.spinnerUserSkillLevel),
-                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                allOf(withId(android.R.id.message), withText("Would you like to book for: Date: 11/7/2020 At the following time slots:\n Time Slot 1: 1\n Time Slot 2: 0\n Time Slot 3: 0\n Time Slot 4: 0"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
                         isDisplayed()));
-        textView.check(matches(withText("2")));
+        textView.check(matches(withText(containsString("Time Slot 1: 1"))));
+
+        ViewInteraction button3 = onView(
+                allOf(withId(android.R.id.button1), withText("Yes"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        button3.perform(scrollTo(), click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.booking_confirmation_text2), withText("Booking made for \n Date: 11/7/2020\n At the following time slots:\n Time Slot 1: 1\n Time Slot 2: 0\n Time Slot 3: 0\n Time Slot 4: 0"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText(containsString("Time Slot 1: 1"))));
+        org.junit.Assert.assertNotNull(textView2);
     }
 
     private static Matcher<View> childAtPosition(
